0dc05c8c71cd58aa5a04d7331c8b8d1da539ec47
Irene Teresa <ireneteresa13@gmail.com>
2023-10-01 02:47:00 +0100

    alteraçoes basicas

--- script.js
+++ script.js
@@ -14,27 +14,30 @@ if (menuButton) {
 }
 
 
-    ready(function(){
-        $('.slick-carousel').slick({
-            slidesToShow: 3, 
-            slidesToScroll: 1,
-            autoplay: true, 
-            autoplaySpeed: 2000, 
-            responsive: [
-                {
-                    breakpoint: 768, 
-                    settings: {
-                        slidesToShow: 2, 
-                    }
-                },
-                {
-                    breakpoint: 480, 
-                    settings: {
-                        slidesToShow: 1, 
-                    }
+    // carrossel
+
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        slidesToShow: 3, 
+        slidesToScroll: 1,
+        autoplay: true, 
+        autoplaySpeed: 2000, 
+        responsive: [
+            {
+                breakpoint: 768, 
+                settings: {
+                    slidesToShow: 2, 
                 }
-            ]
-        });
+            },
+            {
+                breakpoint: 480, 
+                settings: {
+                    slidesToShow: 1, 
+                }
+            }
+        ]
     });
+});
+
 
 
